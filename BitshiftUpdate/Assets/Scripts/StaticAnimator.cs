e2b87c48e14afc07eedfbbbba19fb5637038005f
michael121yang <michael.yang@tufts.edu>
2016-07-25 15:13:50 -0400

    Other Static Things Bitshift
    Sky, floating ground and hills now bitshift
    
    Added "staticanimator" script to control further non-interactive
    bitshifting objects

---
+++ BitshiftUpdate/Assets/Scripts/StaticAnimator.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StaticAnimator : MonoBehaviour {
+
+	private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		animateObject(GameManager.Instance.bitshifted);
+	}
+
+	void animateObject(bool shifted)
+	{
+		if (shifted)
+			anim.SetBool ("shifted", true);
+		else
+			anim.SetBool ("shifted", false);
+	}
+}
