4b407fac63bfafab0725254562ae5dac1deb7a92
David Taus <david.taus@tufts.edu>
2016-07-26 18:10:31 -0400

    cloud animation implemented

---
+++ BitshiftUpdate/Assets/Scripts/CloudController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloudController : MonoBehaviour {
+
+	private Animator anim;
+
+	void Start () 
+	{
+		anim = GetComponent<Animator> ();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		animateClouds (GameManager.Instance.bitshifted);
+	
+	}
+
+	void animateClouds(bool shifted)
+	{
+		anim.SetBool("shifted", shifted);
+	}
+}
