1d4e127c087b0dd12af39c80d1fc24448c8422b9
michael121yang <michael.yang@tufts.edu>
2016-07-26 23:02:28 -0400

    Basic Music and Sound Added
    music is child of main camera, jump sounds child of player.

---
+++ BitshiftUpdate/Assets/Scripts/AudioController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioController : MonoBehaviour {
+
+	public AudioSource normal;
+	public AudioSource shifted;
+
+	// Use this for initialization
+	void Start () {
+		normal.mute = false;
+		shifted.mute = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (GameManager.Instance.bitshifted) {
+			normal.mute = true;
+			shifted.mute = false;			
+		} else {
+			normal.mute = false;
+			shifted.mute = true;
+		}
+	}
+}
