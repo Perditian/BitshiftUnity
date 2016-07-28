0bcf6e0707c61ae3528675b31775a9b2b5d283db
michael121yang <michael.yang@tufts.edu>
2016-07-28 12:51:53 -0400

    Revert "Bullet Work"
    This reverts commit 95cab8125d66d502e0f3e3e25a94418b86a003a2.

--- BitshiftUpdate/Assets/Scripts/BulletController.cs
+++ BitshiftUpdate/Assets/Scripts/BulletController.cs
@@ -4,8 +4,6 @@ using System.Collections;
 public class BulletController : MonoBehaviour {
 
 	public Collider2D target; 
-	public float normal_speed;
-	public float shifted_speed;
 	private Rigidbody2D rb2d; 
 	private Renderer sprite;
 
@@ -14,32 +12,19 @@ public class BulletController : MonoBehaviour {
 		rb2d = GetComponent<Rigidbody2D> ();
 		sprite = GetComponent<Renderer> ();
 		Vector2 direction = target.transform.position + new Vector3(.185f, -.325f) - transform.position;
-		if (GameManager.Instance.bitshifted) {
-			rb2d.velocity = direction.normalized * shifted_speed;
-		} else {
-			rb2d.velocity = direction.normalized * normal_speed;
-		}
+		rb2d.velocity = direction.normalized * 10;
 	}
 	
 	void OnTriggerEnter2D(Collider2D hit) {
 		if (hit.gameObject.tag == "Player") {
 			Debug.Log("YOU DEAD");
 		}
-		Destroy(this.gameObject);
 	}
 
 	// Update is called once per frame
 	void Update () {
 		if (!sprite.isVisible) {
-			Destroy(this.gameObject);
+			Destroy(this);
 		}
 	}
-
-	void FixedUdate () {
-		if (GameManager.Instance.bitshifted) {
-			rb2d.velocity = rb2d.velocity.normalized * shifted_speed;
-		} else {
-			rb2d.velocity = rb2d.velocity.normalized * normal_speed;
-		}
-	}
-} 
+}
